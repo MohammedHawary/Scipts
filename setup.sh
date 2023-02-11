c3f00e5f4ef3521f0c6333a19612087b3db7b3ae
MohammedHawary <94152045+MohammedHawary@users.noreply.github.com>
2023-02-11 13:05:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-pip install -r "requerd.txt"
+pip install -r "requirements.txt"
 chmod 777 *
 mv setg /bin
 mv interactive_shell /bin
